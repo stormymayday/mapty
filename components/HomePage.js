50cd39b98bd51956a9dc932c4cd6bcd3ec20815b
stormymayday <stormymayday7@gmail.com>
2023-10-11 21:56:08 -0700

    Testing facingMode: 'environment'

--- components/HomePage.js
+++ components/HomePage.js
@@ -67,7 +67,21 @@ export default class HomePage extends HTMLElement {
         // Custom Polyfills - End
 
         // Getting access to the device camera
-        navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+        navigator.mediaDevices.getUserMedia({
+            video: {
+                width: {
+                    min: 1280,
+                    ideal: 1920,
+                    max: 2560,
+                },
+                height: {
+                    min: 720,
+                    ideal: 1080,
+                    max: 1440
+                },
+                facingMode: 'environment'
+            }
+        }).then((stream) => {
 
             // Access granted
             // Outputting the stream
