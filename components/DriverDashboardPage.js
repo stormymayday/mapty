6b9a7d231b0b52f4c353f642ef8fcbc69dbf5ba1
stormymayday <stormymayday7@gmail.com>
2023-11-01 23:18:42 -0700

    Testing tyleLater styles

--- components/DriverDashboardPage.js
+++ components/DriverDashboardPage.js
@@ -32,8 +32,10 @@ export default class DriverDashboardPage extends HTMLElement {
 
             // var map = L.map('map', { dragging: !L.Browser.mobile, tap: !L.Browser.mobile });
 
+            // Testing tileLayer styles
+
             // Tilelayer
-            L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(this.map);
 
